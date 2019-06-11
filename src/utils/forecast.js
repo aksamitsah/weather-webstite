6338061eec258f7cf52f2a7a5b395841eab70389
Amit Sah <aksamitsah098@gmail.com>
2019-06-11 22:22:34 +0530

    setup run on local machine nodemon

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,9 +10,10 @@ const forecast = (latitude, longitude, callback) => {
             if (body.error) {
                 callback('unable to find coordinate', undefined)
             }
-            else {
-                callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            else  {
+                callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
             }
+    
         }
     })
 }
